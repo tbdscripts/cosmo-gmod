8c5ec2c346fab804f357a8bb71afd03f00c32ce8
TomDotBat <tom@tomdotbat.dev>
2020-12-19 11:58:25 +0000

    Optimise the purchase notification networking

--- lua/cosmo/network/cl_network.lua
+++ lua/cosmo/network/cl_network.lua
@@ -1,7 +1,7 @@
 net.Receive("Cosmo.PurchaseNotification", function()
   local ply = net.ReadEntity()
   local packageName = net.ReadString()
-  if not IsValid(ply) or not packageName then return end
+  if not (IsValid(ply) and packageName) then return end
 
   Cosmo:pushNotification(ply, packageName)
 end)
