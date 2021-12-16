9c36c4e31de94b84d6cff5abb90a987afe18c88f
Mex de Loo <mex@zeodev.cc>
2021-12-16 11:42:17 +0100

    add: config hints

--- lua/cosmo/sh_config.lua
+++ lua/cosmo/sh_config.lua
@@ -1,16 +1,42 @@
+-- LEAVE THIS LINE
 Cosmo.Config = Cosmo.Config or {}
+-- LEAVE LINE ABOVE
 
+
+--[[
+    Instance Url
+        - This is your site url, so for example our demo is "https://demo.tbdscripts.com"
+        - Notice that there is NO trailing paths like /store or /forums
+]]
 Cosmo.Config.InstanceUrl = "https://your.domain"
 
+--[[
+    Donate Command
+        - The command which opens your store
+]]
 Cosmo.Config.DonateCommand = "!donate"
 
+--[[
+    Notification Time
+        - The time for which one notification stays visible
+]]
 Cosmo.Config.NotificationTime = 3
 
+--[[
+    Notification Theme
+        - The colors and rounding options for the notifications
+]]
 Cosmo.Config.NotificationTheme = {
     Background = Color(30, 30, 30),
     Header = Color(40, 40, 40),
+
+    Roundness = 6,
 }
 
+--[[
+    Language
+        - Translation strings, mainly for the notification
+]]
 Cosmo.Config.Language = {
     NotificationTitle = "Store Purchase",
     NotificationContent = ":player has purchased :package!"
--- lua/cosmo/sv_config.lua
+++ lua/cosmo/sv_config.lua
@@ -1,5 +1,18 @@
+-- LEAVE THIS LINE
 Cosmo.Config = Cosmo.Config or {}
+-- LEAVE LINE ABOVE
 
+
+--[[
+    Server Token
+        - This token can be found (and regenerated) at Management -> Components -> Servers
+        - Tokens are unique per server!
+        - Do not share this token with anyone!
+]]
 Cosmo.Config.ServerToken = "<your token>"
 
+--[[
+    Fetch Interval
+        - The interval at which the addon checks for new purchases
+]]
 Cosmo.Config.FetchInterval = 60
