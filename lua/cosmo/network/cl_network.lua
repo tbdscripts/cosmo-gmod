60df3640a9844f0dd9e711bae47b047e95584fd2
Mex de Loo <mex@zeodev.cc>
2021-12-18 16:41:58 +0100

    fix: use "instUrl" variable

--- lua/cosmo/network/cl_network.lua
+++ lua/cosmo/network/cl_network.lua
@@ -3,5 +3,5 @@ local trimRight = string.TrimRight
 net.Receive("Cosmo.DonateCommand", function()
     local instUrl = trimRight(Cosmo.Config.InstanceUrl, "/")
 
-    gui.OpenURL(Cosmo.Config.InstanceUrl .. "/store")
+    gui.OpenURL(instUrl .. "/store")
 end)
