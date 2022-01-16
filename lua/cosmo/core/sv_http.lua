9f559eec066cda6759ef17cef4ed855a42d6a80f
George Stubbs <georgestubbs24@gmail.com>
2022-01-16 15:53:38 +0000

    Added status code check
    lets not say success when it fails 🙄

--- lua/cosmo/core/sv_http.lua
+++ lua/cosmo/core/sv_http.lua
@@ -27,6 +27,28 @@ function HTTP_CLIENT:SetAuthorizationToken(token)
     self.Defaults.Headers["Authorization"] = "Bearer " .. token
 end
 
+local function httpSuccess(code, body, headers)
+    if code != 200 then
+        return httpFailed("Returned invalid response code: " .. code)
+    end
+    
+    Cosmo.Log.Debug("(HTTP)", "Request succeeded, status code:", code)
+    Cosmo.Log.Debug("Endpoint:", url)
+
+    if body and headers["Content-Type"] == "application/json" then
+        body = util.JSONToTable(body)
+    end
+
+    promise:Resolve(body, code, headers)
+end
+
+local function httpFailed(reason)
+    Cosmo.Log.Danger("(HTTP)", "Request failed with reason:", reason)
+    Cosmo.Log.Danger("Endpoint:", url)
+
+    promise:Reject(reason)
+end
+
 function HTTP_CLIENT:DoRequest(verb, endpoint, data, headers)
     verb = upper(verb)
     headers = mergeTable(headers or {}, self.Defaults.Headers)
@@ -45,23 +67,8 @@ function HTTP_CLIENT:DoRequest(verb, endpoint, data, headers)
         body = data,
         type = headers["Content-Type"],
 
-        failed = function(reason)
-            Cosmo.Log.Danger("(HTTP)", "Request failed with reason:", reason)
-            Cosmo.Log.Danger("Endpoint:", url)
-
-            promise:Reject(reason)
-        end,
-
-        success = function(code, body, headers)
-            Cosmo.Log.Debug("(HTTP)", "Request succeeded, status code:", code)
-            Cosmo.Log.Debug("Endpoint:", url)
-
-            if body and headers["Content-Type"] == "application/json" then
-                body = util.JSONToTable(body)
-            end
-
-            promise:Resolve(body, code, headers)
-        end,
+        failed = httpFailed,
+        success = httpSuccess,
     })
 
     return promise
